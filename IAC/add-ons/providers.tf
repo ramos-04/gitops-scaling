2f09df961706413cd7abf4522a6db66d10959e13
ramos-04 <65966722+ramos-04@users.noreply.github.com>
2025-07-15 11:05:26 +0530

    Update providers.tf

--- IAC/add-ons/providers.tf
+++ IAC/add-ons/providers.tf
@@ -1,25 +1,25 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+          }
     kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23"
-    }
+                    source  = "hashicorp/kubernetes"
+                    version = "~> 2.23"
+                 }
 
-  }
-}
+         }
+       }
 
 provider "aws" {
-  region = var.aws_region
-}
+                  region = var.aws_region
+               }
 
 provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  #cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
-  #token                  = var.cluster_auth_token
-  config_path = "~/.kube/config"
-}
+                          host                   = var.cluster_endpoint # endpoint access url of the EKS cluster
+                          #cluster_ca_certificate = base64decode(var.cluster_certificate_authority_data)
+                          #token                  = var.cluster_auth_token
+                          config_path = "~/.kube/config" # Used for authentication
+                      }
 
